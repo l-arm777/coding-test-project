06c764b23eda136953741f53b23f7752950bed08
l-arm777 <rosalindalee777@gmail.com>
2024-09-04 01:20:06 +0900

    refactor: modify ⓕset_array

--- 02-string/boj_1919.py
+++ 02-string/boj_1919.py
@@ -5,19 +5,22 @@ input1 = input()
 input2 = input()
 
 output = 0
-alphabet_array1, alphabet_array2 = [[0] * (ord('z') - ord('a') + 1) for _ in range(2)]
-# alphabet_array1 = [0] * (ord('z') - ord('a')); alphabet_array2 = [0] * (ord('z') - ord('a')); can be detailed
+# <abbr> alphabet_array1, alphabet_array2 = [[0] * (ord('z') - ord('a') + 1) for _ in range(2)]
+# <detail> alphabet_array1 = [0] * (ord('z') - ord('a')); alphabet_array2 = [0] * (ord('z') - ord('a')); can be detailed
 # caution "+1"
 
 
-def set_array(alphabet_array, user):
+def set_array(user):
+    alphabet_array = [0] * (ord('z') - ord('a') + 1)
     for elem in ''.join(dict.fromkeys(user)):
         index = ord(elem) - ord('a')
         alphabet_array[index] += user.count(elem)
 
+    return alphabet_array
 
-set_array(alphabet_array1, input1)
-set_array(alphabet_array2, input2)
+
+alphabet_array1 = set_array(input1)
+alphabet_array2 = set_array(input2)
 
 """
 for elem in ''.join(dict.fromkeys(input1)):
