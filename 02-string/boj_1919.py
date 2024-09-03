bbb6508bcc073c731f812cb7803c1440464c9f5f
l-arm777 <rosalindalee777@gmail.com>
2024-09-03 12:07:41 +0900

    resolve: boj_1919

---
+++ 02-string/boj_1919.py
@@ -0,0 +1,27 @@
+# 문자열 內 중복제거O, 순서보장X -> ''.join(dict.fromkeys(str)) return 문자열
+# a-z 갯수 -> 26개
+# IndexError ∵ 두 단어 길이가 같을 거라는 보장이 없음
+input1 = input()
+input2 = input()
+
+output = 0
+alphabet_array1, alphabet_array2 = [[0] * (ord('z') - ord('a') + 1) for _ in range(2)]
+# alphabet_array1 = [0] * (ord('z') - ord('a')); alphabet_array2 = [0] * (ord('z') - ord('a')); can be detailed
+# caution "+1"
+
+
+def set_array(alphabet_array, user):
+    for elem in ''.join(dict.fromkeys(user)):
+        index = ord(elem) - ord('a')
+        alphabet_array[index] += user.count(elem)
+
+
+set_array(alphabet_array1, input1)
+set_array(alphabet_array2, input2)
+
+for idx in range(len(alphabet_array1)):
+    diff = abs(alphabet_array1[idx] - alphabet_array2[idx])
+    if diff > 0:
+        output += diff
+
+print(output)
---
+++ practice.py
@@ -0,0 +1,4 @@
+print(ord('z'))  # 122
+print(ord('a'))  # 97
+print(ord('z') - ord('a'))  # 25
+print(''.join(dict.fromkeys("aaabbbccc")))  # abc
