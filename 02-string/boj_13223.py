9cdd33ee27df33cc6a0e6b15a032a043b626557e
l-arm777 <rosalindalee777@gmail.com>
2024-09-04 17:18:33 +0900

    unresolved: boj_13223

---
+++ 02-string/boj_13223.py
@@ -0,0 +1,53 @@
+# default: current < future but after one day: current > future
+# -> ⓐ 24:00:00 - current || ⓑ future
+# 시간 빌림 계산 (current보다 future의 hour은 크지만 min, sec은 작을 때)
+# -> sec이 작으면 ⓐ min-1 ⓑ future_sec + 60 ⓒ 계산진행 / min이 작으면 ⓐ hour-1 ⓑ future_min+ 60 ⓒ 계산진행
+# idx 0[hour, 시] 1[minute, 분], 2[second, 초]
+def calculate_time(minuend, subtrahend):
+    # minuend - subtrahend
+    result = [0] * 3
+
+    # second 먼저 계산
+    result[2] = minuend[2] - subtrahend[2]
+    if minuend[2] < subtrahend[2]:
+        minuend[1] -= 1
+        result[2] += 60
+
+    # minute 계산
+    result[1] = minuend[1] - subtrahend[1]
+    if minuend[1] < subtrahend[1]:
+        minuend[0] -= 1
+        result[1] += 60
+
+    result[0] = minuend[0] - subtrahend[0]
+
+    return result
+
+
+current = list(map(int, input().split(':')))
+future = list(map(int, input().split(':')))
+output = [0, 0, 0]
+
+# print(future, current)
+# print(calculate_time(future, current))
+
+# 하루가 지났을 경우
+if future[0] < current[0] or (future[0] == current[0] and future[1] < current[1]):
+    noon = [24, 0, 0]
+
+    yesterday = calculate_time(noon, current)
+
+    for i in range(3):
+        output[i] = yesterday[i] + future[i]
+
+    # 계산 상 sec, min이 60을 넘었을 때 => 언제 이런 케이스가 발생할까
+    if output[2] > 60:
+        output[1] += 1
+        output[2] -= 60
+    if output[1] > 60:
+        output[0] += 1
+        output[1] -= 60
+else:
+    output = calculate_time(future, current)
+
+print(':'.join(list(map(lambda x: str(x).zfill(2), output))))
